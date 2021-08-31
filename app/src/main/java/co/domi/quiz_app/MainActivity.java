829a93d431f07c4c8edd42a181c6e1fb4eb60f95
JuanBlanco <juanfelipeblancot@hotmail.com>
2021-08-30 23:24:13 -0500

    Re start method added

--- app/src/main/java/co/domi/quiz_app/MainActivity.java
+++ app/src/main/java/co/domi/quiz_app/MainActivity.java
@@ -2,8 +2,11 @@ package co.domi.quiz_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,9 +19,11 @@ public class MainActivity extends AppCompatActivity {
     private EditText respuesta;
     private TextView puntaje;
     private Button responder;
+    private Button resetBtn;
     private Pregunta preguntaActual;
     private int timeLeft;
     private int score;
+    private int pressedTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,31 +35,17 @@ public class MainActivity extends AppCompatActivity {
         respuesta = findViewById(R.id.respuestaTextfield);
         puntaje = findViewById(R.id.puntajeText);
         responder = findViewById(R.id.responderBtn);
+        resetBtn = findViewById(R.id.reintentoBtn);
 
+        resetBtn.setVisibility(View.GONE);
         score = 0;
         timeLeft = 30;
         preguntaActual = new Pregunta();
         preguntaText.setText(preguntaActual.getPregunta());
 
-        timeCount.setText(""+timeLeft);
+        timeCount.setText("Time left: "+timeLeft);
 
-        new Thread(
-                () -> {
-                    while(timeLeft > 0){
-                        try{
-                            timeLeft--;
-                            runOnUiThread(
-                                    () -> {
-                                        timeCount.setText(""+timeLeft);
-                                    }
-                            );
-                            Thread.sleep(1000);
-                        }catch(Exception e){
-
-                        }
-                    }
-                }
-        ).start();
+        counterThread();
 
         puntaje.setText("Puntaje: "+ score);
 
@@ -63,18 +54,82 @@ public class MainActivity extends AppCompatActivity {
                     verificarRespuesta();
                 }
         );
+
+        resetBtn.setOnClickListener(
+                (view) -> {
+                    resetGame();
+                }
+        );
+    }
+
+    private void counterThread(){
+        new Thread(
+                () -> {
+                    while(timeLeft > 0){
+                        try{
+                            timeLeft--;
+                            runOnUiThread(
+                                    () -> {
+                                        timeCount.setText("Time left: "+timeLeft);
+                                    }
+                            );
+                            Thread.sleep(1000);
+                        }catch(Exception e){
+
+                        }
+                    }
+                    if(timeLeft == 0){
+                        runOnUiThread(
+                                () -> {
+                                    resetBtn.setVisibility(View.VISIBLE);
+                                    responder.setVisibility(View.GONE);
+                                    preguntaText.setText("Tiempo agotado");
+                                    respuesta.setEnabled(false);
+                                }
+                        );
+
+                    }
+                }
+        ).start();
+    }
+
+
+    private void resetGame() {
+        timeLeft = 30;
+        score = 0;
+        resetBtn.setVisibility(View.GONE);
+        responder.setVisibility(View.VISIBLE);
+        respuesta.setEnabled(true);
+        preguntaActual = new Pregunta();
+        preguntaText.setText(preguntaActual.getPregunta());
+       counterThread();
     }
 
     private void verificarRespuesta() {
-        String respuestaS = respuesta.getText().toString();
-        int respuestaInt = (int) Integer.parseInt(respuestaS);
+        String respuestaS;
+        int respuestaInt;
+
+        if(respuesta.getText().equals(null)){
+            respuestaInt = 0;
+        }else{
+            respuestaS = respuesta.getText().toString();
+            respuestaInt = (int) Integer.parseInt(respuestaS);
+        }
+        respuesta.setText("");
+
 
         if (respuestaInt == preguntaActual.getRespuesta()) {
             Toast.makeText(this, "Correcto", Toast.LENGTH_SHORT).show();
-            score+=10;
+            score+=5;
             puntaje.setText("Puntaje: "+score);
         } else {
             Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+            if(score<=4){
+                score = 0;
+            }else {
+                score -= 4;
+            }
+            puntaje.setText("Puntaje: "+score);
         }
         
         generarNuevaPregunta();
